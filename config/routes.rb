871518c3619e935b0c90c91d462f9e5f101205c9
Owen Beatty <owen.dj.beatty@gmail.com>
2020-03-24 22:26:41 -0500

    added additional pages and styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "customers#index"
+
+  get "customers/alphabetized", to: "customers#alphabetized"
+
+  get "customers/missing_email", to: "customers#missing_email"
 end
